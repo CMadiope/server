396e9d5b6148d00a9b141eaac9862b1788f6ee29
Confidence Madiope <confidencem97@gmail.com>
2024-06-28 22:20:17 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImExZGE0YmMyOGFmY2IwYjYwMTU5OGUzN2UxZjVmOTM5In0.e30.sajgDN5qWaeQbuxvcOljOzJ5_i9PfFGv_BQedQh91AzGOemErDDNQblUz4RjNnPGStlslUaXvRvqnpeaK6ohCA",
+};
